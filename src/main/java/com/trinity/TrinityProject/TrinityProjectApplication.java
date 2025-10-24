0bddd4fcd5e0b851ed790693a65c3238bdbe0187
BuildTools <unconfigured@null.spigotmc.org>
2025-10-24 08:55:45 +0200

    proj

---
+++ src/main/java/com/trinity/TrinityProject/TrinityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.trinity.TrinityProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinity/TrinityProject/TrinityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinity.TrinityProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrinityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
